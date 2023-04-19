4b4d6e811873de116dbd36c6e0862bf3d4a9da45
Hagai Kariti <hkariti@gmail.com>
2023-04-19 22:43:39 +0300

    Auto-switch to virtual envs when cd-ing to them

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -2,3 +2,4 @@ zsh-users/zsh-completions
 zlsun/solarized-man
 zsh-users/zsh-syntax-highlighting
 wkentaro/zsh-git-prompt branch:fast-python-gitstatus-using-porcelain
+MichaelAquilina/zsh-autoswitch-virtualenv
